a806bef63f0131f12e31f4f0f68d6699b6b6954c
Иван <ikotskiy@gmail.com>
2024-09-27 22:30:52 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+
+        private void ChangeWindowColor(object sender, RoutedEventArgs e) {
+            MenuItem menuItem = (MenuItem)sender;
+            string color = menuItem.Header.ToString();
+
+            if(color == "Синий") {
+                TXTBLOCK.Background = Brushes.Blue;
+            } else if(color == "Красный") {
+                TXTBLOCK.Background = Brushes.Red;
+            } else if(color == "Зеленый") {
+                TXTBLOCK.Background = Brushes.Green;
+            }
+
+        }
+        private void CloseWindow(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+        private void Text(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+    }
+}
